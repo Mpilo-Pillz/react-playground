2cf80ac09c620814f6afb784c98576e6e5af2237
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-06-02 17:04:00 +0200

    adding routes to app component

--- mui-formik/src/App.tsx
+++ mui-formik/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import Header from "./components/shared/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
