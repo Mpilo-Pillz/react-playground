ced9f0b8c40a6b1b25e42da60f993b2f13629380
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-07-07 22:30:24 +0200

    adding login page elements

--- mui-formik/src/components/auth/login/LoginForm.tsx
+++ mui-formik/src/components/auth/login/LoginForm.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <Box
+      sx={{
+        width: 500,
+        maxWidth: "100%",
+        margin: "auto",
+      }}
+    >
+      <Grid mt={16} container spacing={3} flexDirection={"column"}>
+        <Grid item>
+          <Typography textAlign={"center"} variant="h3">
+            Login
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography textAlign={"center"}>
+            If you have an account please enter your login details
+          </Typography>
+        </Grid>
+        <Grid item>
+          <TextField name="email" fullWidth label="Email" id="fullWidth" />
+        </Grid>
+        <Grid item>
+          <TextField
+            name="password"
+            fullWidth
+            label="Password"
+            id="fullWidth"
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            size="large"
+            fullWidth
+            style={{ backgroundColor: "#0E2954" }}
+          >
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default LoginForm;
