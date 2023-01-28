035311e20cb752e1717eb7d2436cdbb78572a2f2
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-01-28 22:30:24 +0200

    creating register page

---
+++ mui-formik/src/pages/auth/Register.tsx
@@ -0,0 +1,29 @@
+import { Form, Formik } from "formik";
+import React from "react";
+import useRegister from "../../components/auth/register/useRegister";
+import { AuthRequest } from "../../components/auth/types";
+import RegisterForm from "../../components/auth/register/RegisterForm";
+
+const Register = () => {
+  const { initialValues, validationSchema, handleSubmit } = useRegister();
+
+  return (
+    <Formik<Partial<AuthRequest>>
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {() => {
+        return (
+          <>
+            <Form>
+              <RegisterForm />
+            </Form>
+          </>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default Register;
