9f47c3c43a8c8124e9876717c4d46c1d550c6c91
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-02-06 21:30:24 +0200

    creating use http client hook

---
+++ mui-formik/src/components/shared/hooks/useHttpClient.ts
@@ -0,0 +1,56 @@
+import { useCallback, useRef, useState } from "react";
+
+type httpRequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>();
+
+  const activeHttpRequests = useRef<AbortController[]>([]);
+
+  const sendRequest = useCallback(
+    async (
+      url: string,
+      method: httpRequestMethod = "GET",
+      body: string | FormData | null = null,
+      headers = {}
+    ): Promise<any> => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData: { message: string } = await response.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        return responseData;
+      } catch (err: any) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  return { isLoading, error, sendRequest, clearError };
+};
