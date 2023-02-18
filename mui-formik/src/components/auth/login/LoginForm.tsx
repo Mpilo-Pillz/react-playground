16c59e03e70e540e5ec0bfa91dea766d0edb6624
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-02-18 18:59:24 +0200

    wrapping in formik form

--- mui-formik/src/components/auth/login/LoginForm.tsx
+++ mui-formik/src/components/auth/login/LoginForm.tsx
@@ -1,64 +1,75 @@
 import React from "react";
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import useShared from "../../shared/hooks/useShared";
+import { Field, Form } from "formik";
 
 const LoginForm = () => {
   const { navigate } = useShared();
 
   return (
-    <Box
-      sx={{
-        width: 500,
-        maxWidth: "100%",
-        margin: "auto",
-      }}
-    >
-      <Grid mt={16} container spacing={3} flexDirection={"column"}>
-        <Grid item>
-          <Typography textAlign={"center"} variant="h3">
-            Login
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography textAlign={"center"}>
-            If you have an account please enter your login details
-          </Typography>
-        </Grid>
-        <Grid item>
-          <TextField name="email" fullWidth label="Email" id="fullWidth" />
-        </Grid>
-        <Grid item>
-          <TextField
-            name="password"
-            fullWidth
-            label="Password"
-            id="fullWidth"
-          />
-        </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            size="large"
-            fullWidth
-            style={{ backgroundColor: "#0E2954" }}
-          >
-            Login
-          </Button>
-          <Box textAlign={"center"}>
-            <Link
-              sx={{ mt: 2 }}
-              component="button"
-              variant="body2"
-              onClick={() => {
-                navigate("/register");
-              }}
+    <Form>
+      <Box
+        sx={{
+          width: 500,
+          maxWidth: "100%",
+          margin: "auto",
+        }}
+      >
+        <Grid mt={16} container spacing={3} flexDirection={"column"}>
+          <Grid item>
+            <Typography textAlign={"center"} variant="h3">
+              Login
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography textAlign={"center"}>
+              If you have an account please enter your login details
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Field
+              name="email"
+              fullWidth
+              label="Email"
+              id="fullWidth"
+              as={TextField}
+            />
+          </Grid>
+          <Grid item>
+            <Field
+              name="password"
+              fullWidth
+              label="Password"
+              id="fullWidth"
+              as={TextField}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              style={{ backgroundColor: "#0E2954" }}
             >
-              Don't have an Account? Click here to Register
-            </Link>
-          </Box>
+              Login
+            </Button>
+            <Box textAlign={"center"}>
+              <Link
+                sx={{ mt: 2 }}
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  navigate("/register");
+                }}
+              >
+                Don't have an Account? Click here to Register
+              </Link>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Form>
   );
 };
 
