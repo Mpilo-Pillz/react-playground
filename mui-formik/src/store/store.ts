4f3690ffed1146c184485194006c2d628470f2c4
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-03-30 18:59:24 +0200

    adding logout to state

--- mui-formik/src/store/store.ts
+++ mui-formik/src/store/store.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
+import { USERDATA } from "../constants/constants";
 
 interface AppStore {
   isLoggedIn: boolean;
   setIsLoggedIn: () => void;
+  setIsLoggedOut: () => void;
 }
 
 const useStore = create<AppStore>((set) => ({
-  isLoggedIn: true,
-  setIsLoggedIn: () => set((state) => ({ isLoggedIn: state.isLoggedIn })),
+  isLoggedIn: !!localStorage.getItem(USERDATA),
+  setIsLoggedIn: () => set(() => ({ isLoggedIn: true })),
+  setIsLoggedOut: () => set(() => ({ isLoggedIn: false })),
 }));
 
 export default useStore;
