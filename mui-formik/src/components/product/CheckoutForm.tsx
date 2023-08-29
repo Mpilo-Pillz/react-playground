37411638c0f165e741b03d3d53472900a57a6fb9
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-08-29 19:37:43 +0200

    adding isopen

--- mui-formik/src/components/product/CheckoutForm.tsx
+++ mui-formik/src/components/product/CheckoutForm.tsx
@@ -41,7 +41,7 @@ const CheckoutForm = () => {
   return (
     <>
       <Modal
-        open={true}
+        open={isOpen}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
