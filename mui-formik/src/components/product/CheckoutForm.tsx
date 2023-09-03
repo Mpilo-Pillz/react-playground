1817af1c1658b5e8a5290a6682694090504730f2
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-09-03 12:19:39 +0200

    adding field validaion to checkout form

--- mui-formik/src/components/product/CheckoutForm.tsx
+++ mui-formik/src/components/product/CheckoutForm.tsx
@@ -6,6 +6,7 @@ import {
   CardMedia,
   Container,
   Grid,
+  Link,
   MenuItem,
   Modal,
   Select,
@@ -13,7 +14,7 @@ import {
   Typography,
 } from "@mui/material";
 import { Field, Form, useFormikContext } from "formik";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useProductStore } from "../../store/store";
 import useAddress from "../user/useAddress";
 import useCheckout from "./useCheckout";
@@ -21,11 +22,22 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import { ISubscription } from "./productTypes";
+import { IAddress } from "../user/types";
 
-const CheckoutForm = () => {
+interface Props {
+  userAddresses: any;
+  subscriptionSuccessful: boolean;
+  navigate: (route: string) => void;
+}
+const CheckoutForm: React.FC<Props> = ({
+  userAddresses,
+  subscriptionSuccessful,
+  navigate,
+}) => {
   const { getUserAddresses } = useAddress();
-  const { values, setFieldValue, errors } = useFormikContext();
-  const { userAddresses, isOpen, handleClose } = useCheckout();
+  const { values, setFieldValue, touched, errors } =
+    useFormikContext<Partial<ISubscription>>();
   const selectedProduct = useProductStore(
     (state: any) => state.selectedProduct
   );
@@ -40,112 +52,131 @@ const CheckoutForm = () => {
 
   return (
     <>
-      <Modal
-        open={isOpen}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
       <Container sx={{ mt: 3, maxWidth: "400px" }}>
-        <Card sx={{ p: 2 }}>
-          <Form>
-            <Box
-              sx={{
-                maxWidth: "100%",
-                margin: "auto",
-              }}
-            >
-              <CardMedia
-                component="img"
-                alt={selectedProduct.name}
-                image={selectedProduct?.image}
-              />
-              <Grid container spacing={3} flexDirection={"column"}>
-                <Grid item>
-                  {/* {!!error && <Alert severity="error">{error}</Alert>} */}
-                  <Typography textAlign={"center"} variant="h4">
-                    Select Address for installation
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    as={Select}
-                    type="select"
-                    name="address"
-                    fullWidth
-                    label="Address"
-                    id="fullWidth"
-                  >
-                    {userAddresses?.addresses?.map((address: any) => {
-                      return (
-                        <MenuItem key={address._id} value={address._id}>
-                          {`${address.streetNumber} ${address.streetName}`}
-                        </MenuItem>
-                      );
-                    })}
-                  </Field>
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    name="cellphone"
-                    fullWidth
-                    label="MOMO Cellphone Number"
-                    id="fullWidth"
-                    as={TextField}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                      label="Start Date"
-                      onChange={(date) => {
-                        setFieldValue("startDate", date);
-                      }}
-                    />
-                  </LocalizationProvider>
-                </Grid>
-                <Grid item xs={12}>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      <span style={{ color: "rgba(0, 0, 0, 0.6)" }}>
-                        Charge:
-                      </span>{" "}
-                      E {selectedProduct?.price} p/m
-                    </Typography>
-                    <Typography variant="h5" color="text.secondary">
-                      {selectedProduct?.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {selectedProduct?.description}
-                    </Typography>
-                  </CardContent>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    size="large"
-                    fullWidth
-                    style={{ backgroundColor: "#0E2954" }}
-                  >
-                    Add to Invoice
-                  </Button>
-                  {/* <pre>{JSON.stringify(values)}</pre>
-                <pre>{JSON.stringify(errors)}</pre> */}
-                </Grid>
-              </Grid>
+        {subscriptionSuccessful ? (
+          <Card>
+            <Box>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Subscription Successful
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                You have successfully subscribed for this product
+              </Typography>
+              <Box textAlign={"center"}>
+                <Link
+                  sx={{ mt: 2 }}
+                  component="button"
+                  variant="body2"
+                  onClick={() => {
+                    navigate("/profile");
+                  }}
+                >
+                  Go to Profile
+                </Link>
+              </Box>
             </Box>
-          </Form>
-        </Card>
+          </Card>
+        ) : (
+          <Card sx={{ p: 2 }}>
+            <Form>
+              <Box
+                sx={{
+                  maxWidth: "100%",
+                  margin: "auto",
+                }}
+              >
+                <Box textAlign={"center"}>
+                  <img
+                    height={350}
+                    width={350}
+                    alt={selectedProduct.name}
+                    src={selectedProduct?.image}
+                  />
+                </Box>
+
+                <Grid container spacing={3} flexDirection={"column"}>
+                  <Grid item>
+                    {/* {!!error && <Alert severity="error">{error}</Alert>} */}
+                    <Typography textAlign={"center"} variant="h4">
+                      Select Address for installation
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      as={Select}
+                      type="select"
+                      name="address"
+                      fullWidth
+                      label="Address"
+                      id="fullWidth"
+                    >
+                      {userAddresses?.addresses?.map((address: any) => {
+                        return (
+                          <MenuItem key={address._id} value={address._id}>
+                            {`${address.streetNumber} ${address.streetName}`}
+                          </MenuItem>
+                        );
+                      })}
+                    </Field>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      name="cellphone"
+                      type="number"
+                      fullWidth
+                      label="MOMO Cellphone Number"
+                      id="fullWidth"
+                      as={TextField}
+                      error={errors.cellphone}
+                      helperText={
+                        errors.cellphone && touched ? errors.cellphone : ""
+                      }
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker
+                        label="Start Date"
+                        onChange={(date) => {
+                          setFieldValue("startDate", date);
+                        }}
+                      />
+                    </LocalizationProvider>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        <span style={{ color: "rgba(0, 0, 0, 0.6)" }}>
+                          Charge:
+                        </span>{" "}
+                        E {selectedProduct?.price} p/m
+                      </Typography>
+                      <Typography variant="h5" color="text.secondary">
+                        {selectedProduct?.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {selectedProduct?.description}
+                      </Typography>
+                    </CardContent>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      size="large"
+                      fullWidth
+                      style={{ backgroundColor: "#0E2954" }}
+                    >
+                      Add to Invoice
+                    </Button>
+                    {/* <pre>{JSON.stringify(values)}</pre>
+                <pre>{JSON.stringify(errors)}</pre> */}
+                  </Grid>
+                </Grid>
+              </Box>
+            </Form>
+          </Card>
+        )}
       </Container>
     </>
   );
