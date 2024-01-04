02d0e2db309458a39f07a8ed90169a4f99166e0a
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-04 13:05:16 +0200

    adding tailwind forms

--- tailwind-reduxtoolkit-formik/src/App.tsx
+++ tailwind-reduxtoolkit-formik/src/App.tsx
@@ -1,9 +1,7 @@
+import Login from "./pages/auth/Login/Login";
+
 function App() {
-  return (
-    <div className="bg-blue-500 text-white p-4">
-      This is a div with Tailwind CSS classes.
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
--- tailwind-reduxtoolkit-formik/tailwind.config.js
+++ tailwind-reduxtoolkit-formik/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF5722",
+        secondary: "#2196F3",
+        tertiary: "#4CAF50",
+      },
+      screens: {
+        sm: "640px", //  mobile phones
+        md: "768px", //  tablets
+        lg: "1024px", //  laptops/desktops
+        xl: "1280px", //  large desktops
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
