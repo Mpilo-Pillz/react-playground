9358966ad3a7264c615c4d00d380cab05a7712fd
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-07 20:34:00 +0200

    using color palete on register form

--- tailwind-reduxtoolkit-formik/src/components/forms/auth/RegisterForm.tsx
+++ tailwind-reduxtoolkit-formik/src/components/forms/auth/RegisterForm.tsx
@@ -53,7 +53,7 @@ const RegisterForm: React.FC<FormProps> = ({ isValid, loading }) => {
 
         <Button
           type="submit"
-          className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+          className="inline-block rounded-lg bg-secondary px-5 py-3 text-sm font-medium text-white"
           disabled={!isValid || loading}
         >
           Sign up
