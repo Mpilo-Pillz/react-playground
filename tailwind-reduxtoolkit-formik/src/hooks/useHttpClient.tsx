ff86e9c8a4f1e8d540be51a275395de1b9f1ef8a
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-21 20:56:19 +0200

    creating http client hook

---
+++ tailwind-reduxtoolkit-formik/src/hooks/useHttpClient.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useRef, useState } from "react";
+
+type httpRequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [error, setError] = useState<string | null>();
+
+  const activeHttpRequests = useRef<AbortController[]>([]);
+
+  const sendRequest = useCallback(
+    async (
+      url: string,
+      method: httpRequestMethod = "GET",
+      body: string | FormData | null = null,
+      headers = {}
+    ): Promise<any> => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData: { message: string } = await response.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        setIsSuccess(true);
+        return responseData;
+      } catch (err: any) {
+        console.log(err);
+
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    [setIsSuccess]
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  return { isLoading, error, sendRequest, clearError, isSuccess };
+};
