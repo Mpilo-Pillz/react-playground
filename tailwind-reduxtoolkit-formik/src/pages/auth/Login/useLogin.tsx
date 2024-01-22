2a21c8916854b6942e39bd72b7881bcb550cf732
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-22 19:55:03 +0200

    successfully calling login endpoint

--- tailwind-reduxtoolkit-formik/src/pages/auth/Login/useLogin.tsx
+++ tailwind-reduxtoolkit-formik/src/pages/auth/Login/useLogin.tsx
@@ -1,5 +1,15 @@
 import React, { useCallback, useMemo } from "react";
 import * as Yup from "yup";
+import { useHttpClient } from "../../../hooks/useHttpClient";
+
+interface LoginResponse {
+  token: string;
+}
+
+interface LoginCredentials {
+  username: string;
+  password: string;
+}
 
 const useLogin = () => {
   const initialValues = useMemo(
@@ -10,13 +20,34 @@ const useLogin = () => {
     []
   );
 
+  const { sendRequest, error } = useHttpClient();
+
   const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email format").required("Required"),
     password: Yup.string().required("Required!"),
   });
 
-  const handleSubmit = useCallback(() => {
-    alert("TODO implement api call");
+  const login = async (
+    loginCredentials: LoginCredentials
+  ): Promise<LoginResponse> => {
+    const response = await sendRequest(
+      `${import.meta.env.VITE_API_URL}/api/portal/users/login`,
+      "POST",
+      JSON.stringify(loginCredentials),
+      {
+        "Content-Type": "application/json",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+
+    return response.json();
+  };
+
+  const handleSubmit = useCallback((formValues: LoginCredentials) => {
+    login(formValues);
   }, []);
   return { initialValues, validationSchema, handleSubmit };
 };
