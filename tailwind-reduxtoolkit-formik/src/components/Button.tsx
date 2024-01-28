495fde96913ad71b53df7d27f43f17cc48022670
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2024-01-28 22:12:45 +0200

    creating variant buttons

--- tailwind-reduxtoolkit-formik/src/components/Button.tsx
+++ tailwind-reduxtoolkit-formik/src/components/Button.tsx
@@ -1,12 +1,36 @@
 import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
+type Variant = "elementary" | "primary" | "secondary" | "tertiary" | "outline";
+
+interface ButtonProps {
+  variant?: Variant;
+  children: React.ReactNode;
+}
+
 const Button: React.FC<
-  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
-> = (props) => {
-  const { disabled } = props;
+  DetailedHTMLProps<
+    ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+> = ({ variant = "primary", children, disabled, ...rest }) => {
+  const effectiveVariant = disabled ? "tertiary" : variant || "primary";
+
+  const buttonStyles: { [key in Variant]: string } = {
+    elementary: "bg-elementary text-white",
+    primary: "bg-primary text-white",
+    secondary: "bg-secondary text-white",
+    tertiary: "bg-tertiary text-primary",
+    outline:
+      "border border-primary text-primary hover:bg-primary hover:text-white",
+  };
+
   return (
-    <button disabled={disabled} {...props}>
-      {props.children}
+    <button
+      className={`inline-block rounded-lg px-5 py-3 text-sm font-medium ${buttonStyles[effectiveVariant]}`}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
     </button>
   );
 };
